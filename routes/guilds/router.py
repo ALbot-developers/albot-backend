b4c9fb6e38ea4e21357d2340ffc7c01ebee511de
kota113 <kota113@keio.jp>
2024-09-12 20:58:57 +0900

    add settings API router

--- routes/guilds/router.py
+++ routes/guilds/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from routes.guilds import dict_api
+from routes.guilds import dict_api, settings_api
 
 router = APIRouter()
 router.include_router(dict_api.router)
+router.include_router(settings_api.router)
