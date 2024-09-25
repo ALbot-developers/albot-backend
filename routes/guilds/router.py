c9b505a4117fb5576d79f67ea1e9154fafc489c3
kota113 <kota113@keio.jp>
2024-09-25 14:54:02 +0900

    add `connection_states` API router

--- routes/guilds/router.py
+++ routes/guilds/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from routes.guilds import dict_api, settings_api, character_usage_api, trusted_roles_api, connection_command_api, \
-    message_link_expand_pref_api
+    message_link_expand_pref_api, connection_states_api
 
 router = APIRouter()
 router.include_router(dict_api.router)
@@ -9,4 +9,5 @@ router.include_router(settings_api.router)
 router.include_router(character_usage_api.router)
 router.include_router(trusted_roles_api.router)
 router.include_router(connection_command_api.router)
+router.include_router(connection_states_api.router)
 router.include_router(message_link_expand_pref_api.router)
