54257e85dea30d95b29614e2a9bf2a63c6bfbff5
kota113 <kota113@keio.jp>
2024-09-29 19:52:38 +0900

    feat: add guild subscriptions api

--- routes/guilds/router.py
+++ routes/guilds/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from routes.guilds import dict_api, settings_api, character_usage_api, trusted_roles_api, connection_command_api, \
-    message_link_expand_pref_api, connection_states_api
+    message_link_expand_pref_api, connection_states_api, subscriptions_api
 
 router = APIRouter()
 router.include_router(dict_api.router)
@@ -11,3 +11,4 @@ router.include_router(trusted_roles_api.router)
 router.include_router(connection_command_api.router)
 router.include_router(connection_states_api.router)
 router.include_router(message_link_expand_pref_api.router)
+router.include_router(subscriptions_api.router)
