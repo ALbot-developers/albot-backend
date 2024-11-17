8be80994a2a47d3312700f89475d3a43fad30d6d
kota113 <kota113@keio.jp>
2024-11-17 18:01:32 +0900

    fix: restrict fetching/posting premium settings

--- routes/guilds/settings_api.py
+++ routes/guilds/settings_api.py
@@ -1,14 +1,16 @@
 import asyncpg
-from fastapi import APIRouter, Security, HTTPException
+from fastapi import APIRouter, Security, HTTPException, Depends
 
-from models.database import SettingsData
+from models.database import SettingsData, PremiumSettings
+from models.subscription import Subscription
 from utils.auth import verify_all_tokens
 from utils.db_connection import get_connection_pool
+from utils.dependencies import get_subscription
 
 router = APIRouter()
 
 
-async def get_default_settings():
+async def get_default_settings() -> SettingsData:
     async with get_connection_pool().acquire() as conn:
         conn: asyncpg.connection.Connection
         # settings_dataのデフォルト値を取得
@@ -41,7 +43,7 @@ async def get_default_settings():
                 column_default = column_default.replace("'", "")
         column_default = fix_value_type(column_default, row["data_type"])
         default_settings[row["column_name"]] = column_default
-    return default_settings
+    return SettingsData.from_dict(default_settings)
 
 
 async def get_guild_settings(guild_id: int):
@@ -51,13 +53,20 @@ async def get_guild_settings(guild_id: int):
         row = await conn.fetchrow('SELECT * FROM settings_data WHERE guild_id = $1', guild_id)
         # convert the row to a dictionary
         if row is None:
-            return SettingsData.from_dict(await get_default_settings())
+            return await get_default_settings()
         return SettingsData.from_dict(dict(row))
 
 
 @router.get("/{guild_id}/settings")
-async def get_guild_settings_api(guild_id: int, _auth=Security(verify_all_tokens)):
+async def get_guild_settings_api(guild_id: int, subscription: Subscription = Depends(get_subscription),
+                                 _auth=Security(verify_all_tokens)):
     settings = await get_guild_settings(guild_id)
+    if subscription is None:
+        default_settings = await get_default_settings()
+        for key in PremiumSettings.__annotations__.keys():
+            if key not in settings.__dict__:
+                continue
+            setattr(settings, key, getattr(default_settings, key))
     return {
         "message": "Fetched guild data.",
         "data": {
@@ -67,14 +76,20 @@ async def get_guild_settings_api(guild_id: int, _auth=Security(verify_all_tokens
 
 
 @router.post("/{guild_id}/settings")
-async def update_guild_settings(guild_id: int, settings: SettingsData, _auth=Security(verify_all_tokens)):
+async def update_guild_settings(guild_id: int, settings: SettingsData,
+                                subscription: Subscription = Depends(get_subscription),
+                                _auth=Security(verify_all_tokens)):
     async with get_connection_pool().acquire() as conn:
         conn: asyncpg.connection.Connection
         # get all attributes with values
         attributes = {k: (None if v == "" else v) for k, v in settings.__dict__.items() if v is not None}
         if not attributes:
             raise HTTPException(status_code=400, detail="No settings data provided.")
-        # create a query string with placeholders for the attributes
+        # if subscription is None and PremiumSettings in attributes, return an error
+        if subscription is None:
+            if any(k in attributes for k in PremiumSettings.__annotations__.keys()):
+                raise HTTPException(status_code=403, detail="Requires premium subscription.")
+        # noinspection DuplicatedCode
         query = (
             f"INSERT INTO settings_data (guild_id, {', '.join(attributes.keys())}) "
             f"VALUES ({guild_id}, {', '.join([f'${i + 1}' for i in range(len(attributes))])}) "
