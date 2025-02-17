6fa04c7eab2d3e6ded32a9b3909d807889f5a706
kota113 <kota113@keio.jp>
2025-02-18 08:40:51 +0900

    fix: エラーハンドリングを修正

--- app/routes/webhooks/stripe_webhook.py
+++ app/routes/webhooks/stripe_webhook.py
@@ -1,36 +1,36 @@
 import stripe
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, Request
 
 import app.services.webhook
 from app.constants import STRIPE_WEBHOOK_SECRET
+from app.core.error import CustomHTTPException
 from app.schemas.api_response import PlainAPIResponse
 from app.services import webhook
 
 router = APIRouter()
 
 
-async def construct_event(payload: bytes, received_sig: str):
+def construct_event(payload: bytes, received_sig: str):
     try:
         return stripe.Webhook.construct_event(
             payload.decode('utf-8'), received_sig, STRIPE_WEBHOOK_SECRET
         )
     except ValueError:
-        raise HTTPException(400, "Bad payload.")
+        raise CustomHTTPException(400, "Bad payload")
     except stripe.error.SignatureVerificationError:
-        raise HTTPException(400, "Bad signature.")
+        raise CustomHTTPException(400, "Bad signature")
 
 
 @router.post("", response_model=PlainAPIResponse)
 async def stripe_webhook(request: Request):
     payload = await request.body()
     received_sig = request.headers.get("Stripe-Signature")
-    event = await construct_event(payload, received_sig)
+    event = construct_event(payload, received_sig)
     if await app.services.webhook.is_event_duplicated(event):
         # 再送を防ぐために2xxに
-        raise HTTPException(202, "Duplicated event.")
+        raise CustomHTTPException(202, "Duplicate event.")
     if event.type.startswith('customer.subscription'):
         sub_id = event.data['object']['id']
-        # sub_entry = await conn.fetchrow("SELECT * FROM subscriptions WHERE sub_id = $1", sub_id)
         if event.type == 'customer.subscription.deleted':
             await webhook.delete_subscription(sub_id)
             return PlainAPIResponse(message="subscription deleted.")
@@ -38,8 +38,10 @@ async def stripe_webhook(request: Request):
             price_id = event.data['object']['items']['data'][0]["price"]['id']
             await webhook.update_subscription(sub_id, price_id)
             return PlainAPIResponse(message="subscription updated.")
+        else:
+            raise CustomHTTPException(422, "Unexpected event type.")
     elif event.type == 'checkout.session.completed':
         await webhook.handle_checkout_completed(event)
         return PlainAPIResponse(message="subscription created.")
     else:
-        return HTTPException(422, "Unexpected event type.")
+        raise CustomHTTPException(422, "Unexpected event type.")
