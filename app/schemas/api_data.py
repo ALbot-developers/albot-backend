419c0fc36208a0fe1530e4fa71dbff7f4fcde060
kota113 <kota113@keio.jp>
2025-08-11 18:28:13 +0900

    refactor: set explicit `BaseModel` type annotations for schema models

--- app/schemas/api_data.py
+++ app/schemas/api_data.py
@@ -1,4 +1,4 @@
-from pydantic import create_model, Field
+from pydantic import create_model, Field, BaseModel
 
 from app.external.discord.models import UserPIIResponse, PartialGuild
 from app.models.settings import GuildSettings, DefaultSettings
@@ -7,14 +7,14 @@ from app.schemas.connection_state import ConnectionState
 from app.schemas.metrics import Metrics
 
 # todo: connection_state(単数形)に修正
-ConnectionStateData = create_model("ConnectionStateDataModel", connection_states=(ConnectionState, ...))
-DictData = create_model("DictDataModel", dict_=(dict, Field(..., alias="dict")))
-GuildSettingsData = create_model("GuildSettingsDataModel", settings=(GuildSettings, ...))
-SubscriptionsData = create_model("SubscriptionsDataModel", subscriptions=(list[Subscription], ...))
-MetricsData = create_model("MetricsDataModel", metrics=(Metrics, ...))
-ShardConnectionCommandsData = create_model("ShardConnectionCommandsDataModel", commands=(dict, ...))
-ShardsListData = create_model("ShardsListDataModel", ids=(list[int], ...))
-UserInfoData = create_model("UserInfoDataModel", info=(UserPIIResponse, ...))
-GuildsListData = create_model("GuildsListDataModel", guilds=(list[PartialGuild], ...))
-GuildInfoData = create_model("GuildInfoData", info=(PartialGuild, ...))
-URLData = create_model("URLDataModel", url=(str, ...))
+ConnectionStateData: type[BaseModel] = create_model("ConnectionStateDataModel", connection_states=(ConnectionState, ...))
+DictData: type[BaseModel] = create_model("DictDataModel", dict_=(dict, Field(..., alias="dict")))
+GuildSettingsData: type[BaseModel] = create_model("GuildSettingsDataModel", settings=(GuildSettings, ...))
+SubscriptionsData: type[BaseModel] = create_model("SubscriptionsDataModel", subscriptions=(list[Subscription], ...))
+MetricsData: type[BaseModel] = create_model("MetricsDataModel", metrics=(Metrics, ...))
+ShardConnectionCommandsData: type[BaseModel] = create_model("ShardConnectionCommandsDataModel", commands=(dict, ...))
+ShardsListData: type[BaseModel] = create_model("ShardsListDataModel", ids=(list[int], ...))
+UserInfoData: type[BaseModel] = create_model("UserInfoDataModel", info=(UserPIIResponse, ...))
+GuildsListData: type[BaseModel] = create_model("GuildsListDataModel", guilds=(list[PartialGuild], ...))
+GuildInfoData: type[BaseModel] = create_model("GuildInfoData", info=(PartialGuild, ...))
+URLData: type[BaseModel] = create_model("URLDataModel", url=(str, ...))
