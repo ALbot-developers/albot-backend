b323891ae77ae89f71177d9d0b4d240c1420ef6c
kota113 <kota113@keio.jp>
2025-12-06 16:22:56 +0900

    feat: validate inputs to `POST /guilds/[id]/settings`

--- app/schemas/guild_settings.py
+++ app/schemas/guild_settings.py
@@ -1,14 +1,15 @@
-from typing import Optional, Literal
+from typing import Optional, Literal, Annotated
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+GoogleTTSVoiceCode = Annotated[str, Field(pattern=r'^[a-z]{2}-[A-Z]{2,3}-(Wavenet|Standard)-[A-Za-z]$')]
 
 class GuildSettingsUpdate(BaseModel):
     lang: Optional[str] = None
     character_limit: Optional[int] = None
     speech_speed: Optional[float] = None
     read_name: Optional[bool] = None
-    custom_voice: Optional[str] = None
+    custom_voice: Optional[GoogleTTSVoiceCode] = None
     translate: Optional[bool] = None
     read_name_on_join: Optional[bool] = None
     read_name_on_leave: Optional[bool] = None
